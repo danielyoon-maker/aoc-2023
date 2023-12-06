9b2930e213280dc26623ff6598b5b77a6b79dadc
Daniel Yoon <dyyoon02@gmail.com>
2023-12-05 22:58:41 -0800

    day06 in kotlin

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,6 +1,11 @@
+package day01
+
+import println
+import readInput
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+       return input.size
     }
 
     fun part2(input: List<String>): Int {
@@ -8,8 +13,8 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("resources/day01/day01_test1")
+    check(part1(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
---
+++ src/day06/Day06.kt
@@ -0,0 +1,68 @@
+package day06
+
+import readInput
+
+fun main() {
+    fun winningCount(raceTime: Long, distanceToBeat: Long): Long {
+        var count = 0L
+        for (buttonHeld in 0..raceTime) {
+            val distance = buttonHeld * (raceTime - buttonHeld)
+            if (distance > distanceToBeat) {
+                count += 1
+            }
+        }
+        return count
+    }
+
+    fun part1(input: List<String>): Long {
+        val times = input[0].split(":")[1].trim().split("\\s+".toRegex()).map { charInt -> charInt.toInt() }
+        val distances = input[1].split(":")[1].trim().split("\\s+".toRegex()).map { charInt -> charInt.toInt() }
+
+        var winningTotal = 1L
+        times.indices.forEach { idx ->
+            run {
+                val raceTime = times[idx].toLong()
+                val distanceToBeat = distances[idx].toLong()
+                winningTotal *= winningCount(raceTime, distanceToBeat)
+            }
+        }
+        return winningTotal
+    }
+
+    fun findFirstWin(time: Long, distanceToBeat: Long): Long {
+        for (buttonHeld in 0..<time) {
+            val distance = buttonHeld * (time - buttonHeld)
+            if (distance > distanceToBeat) {
+                return buttonHeld
+            }
+        }
+        throw IllegalStateException("Expected win")
+    }
+
+    fun findLastWin(time: Long, distanceToBeat: Long): Long {
+        for (timeRacing in 0..<time) {
+            val distance = timeRacing * (time - timeRacing)
+            if (distance > distanceToBeat) {
+                return (time - timeRacing)
+            }
+        }
+        throw IllegalStateException("Expected win")
+    }
+
+    fun part2(input: List<String>): Long {
+        val time = input[0].split(":")[1].replace(" ", "").toLong()
+        val distanceToBeat = input[1].split(":")[1].replace(" ", "").toLong()
+        return findLastWin(time, distanceToBeat) - findFirstWin(time, distanceToBeat) + 1
+    }
+
+    val testInput = readInput("resources/day06/test1")
+    val realInput = readInput("resources/day06/input")
+
+    check(part1(testInput)==288L)
+
+    println(part1(realInput))
+
+    check(part2(testInput)==71503L)
+
+    print(part2(realInput))
+}
